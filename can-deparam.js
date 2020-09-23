8bcde0d4613a0edf1b2d8b5fc003287504b16443
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2020-09-23 18:32:27 +0100

    Docuement for CodePen

--- can-deparam.js
+++ can-deparam.js
@@ -1,42 +1,6 @@
 "use strict";
 var namespace = require("can-namespace");
 
-/**
- * @module {function} can-deparam can-deparam
- * @parent can-routing
- * @collection can-infrastructure
- * @package ./package.json
- * @description Deserialize a query string into an array or object.
- * @signature `deparam(params)`
- *
- * @param {String} params A form-urlencoded string of key-value pairs.
- * @param {function} [valueDeserializer] A function that decodes the string values. For example, using
- * [can-string-to-any] will convert `"null"` to `null` like:
- *
- *   ```js
- *   import stringToAny from "can-string-to-any";
- *   deparam("value=null", stringToAny) //-> {value: null}
- *   ```
- * @return {Object} The params formatted into an object
- *
- * Takes a string of name value pairs and returns a Object literal that represents those params.
- *
- * ```js
- * var deparam = require("can-deparam");
- *
- * console.log(JSON.stringify(deparam("?foo=bar&number=1234"))); // -> '{"foo" : "bar", "number": 1234}'
- * console.log(JSON.stringify(deparam("#foo[]=bar&foo[]=baz"))); // -> '{"foo" : ["bar", "baz"]}'
- * console.log(JSON.stringify(deparam("foo=bar%20%26%20baz"))); // -> '{"foo" : "bar & baz"}'
- * ```
- * @body
- *
- * ## Try it
- *
- * Use this JS Bin to play around with this package:
- *
- * <a class="jsbin-embed" href="https://jsbin.com/mobimok/3/embed?js,console">can-deparam on jsbin.com</a>
- * <script src="https://static.jsbin.com/js/embed.min.js?4.0.4"></script>
- */
 var digitTest = /^\d+$/,
 	keyBreaker = /([^\[\]]+)|(\[\])/g,
 	paramTest = /([^?#]*)(#.*)?$/,
